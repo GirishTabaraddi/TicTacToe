cb2770e4746d396d51dc8ec160558e5712a57676
Girish Tabaraddi <girishtabaraddi@gmail.com>
2025-11-30 00:06:07 +0100

    updating doxygen file to omit latex generation, only html is enough.

--- TicTacToe/myCode/html/search/all_0.js
+++ TicTacToe/myCode/html/search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['cboard_0',['CBoard',['../class_c_board.html',1,'']]]
+  ['cboard_0',['cboard',['../class_c_board.html',1,'CBoard'],['../class_c_board.html#ad40f85c45d4fa99bee212ba0bcd7edc2',1,'CBoard::CBoard()']]],
+  ['cdisplay_1',['CDisplay',['../class_c_display.html',1,'']]]
 ];
