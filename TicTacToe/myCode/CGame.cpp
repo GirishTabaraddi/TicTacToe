c42289711fd5444ffd629af51505e3426a1e941e
Girish Tabaraddi <girishtabaraddi@gmail.com>
2025-12-01 00:19:25 +0100

    adding game class

---
+++ TicTacToe/myCode/CGame.cpp
@@ -0,0 +1,20 @@
+/*!
+ * CGame.cpp
+ *
+ *  Created on: 01-Dec-2025
+ *      Author: giris
+ */
+
+#include "CGame.h"
+
+CGame::CGame()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+CGame::~CGame()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ TicTacToe/myCode/CGame.h
@@ -0,0 +1,18 @@
+/*!
+ * CGame.h
+ *
+ *  Created on: 01-Dec-2025
+ *      Author: giris
+ */
+
+#ifndef CGAME_H_
+#define CGAME_H_
+
+class CGame
+{
+public:
+	CGame();
+	virtual ~CGame();
+};
+
+#endif /* CGAME_H_ */
